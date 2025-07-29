383f2780bb98f424e54df4f4c8e2279ee6821171
AnilJagari <aniljagari111@gmail.com>
2025-07-29 07:32:26 +0530

    re witted the files

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-
-
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello():
-    return "Hello from Flask CI/CD!"
-
+def home():
+    return "Hello from Flask CI/CD Pipeline!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_index():
-    tester = app.test_client()
-    response = tester.get("/")
-    assert response.status_code == 200
-    assert b"Hello from Flask" in response.data
